8be6566dac23f9c29c51c3bc6476983c86fec93a
alanbjohnston <alan.b.johnston@gmail.com>
2022-07-02 08:44:17 -0400

    Create Arduino-APRS.h

---
+++ Arduino-APRS.h
@@ -0,0 +1,102 @@
+#include <math.h>
+#include <stdio.h>
+
+// Defines the Square Wave Output Pin
+#define OUT_PIN 26
+
+#define _1200   1
+#define _2400   0
+
+#define _FLAG       0x7e
+#define _CTRL_ID    0x03
+#define _PID        0xf0
+#define _DT_EXP     ','
+#define _DT_STATUS  '>'
+#define _DT_POS     '!'
+
+#define _FIXPOS         1
+#define _STATUS         2
+#define _FIXPOS_STATUS  3
+
+bool nada = _2400;
+
+/*
+ * SQUARE WAVE SIGNAL GENERATION
+ * 
+ * baud_adj lets you to adjust or fine tune overall baud rate
+ * by simultaneously adjust the 1200 Hz and 2400 Hz tone,
+ * so that both tone would scales synchronously.
+ * adj_1200 determined the 1200 hz tone adjustment.
+ * tc1200 is the half of the 1200 Hz signal periods.
+ * 
+ *      -------------------------                           -------
+ *     |                         |                         |
+ *     |                         |                         |
+ *     |                         |                         |
+ * ----                           -------------------------
+ * 
+ *     |<------ tc1200 --------->|<------ tc1200 --------->|
+ *     
+ * adj_2400 determined the 2400 hz tone adjustment.
+ * tc2400 is the half of the 2400 Hz signal periods.
+ * 
+ *      ------------              ------------              -------
+ *     |            |            |            |            |
+ *     |            |            |            |            |            
+ *     |            |            |            |            |
+ * ----              ------------              ------------
+ * 
+ *     |<--tc2400-->|<--tc2400-->|<--tc2400-->|<--tc2400-->|
+ *     
+ */
+const float baud_adj = 0.975;
+const float adj_1200 = 1.0 * baud_adj;
+const float adj_2400 = 1.0 * baud_adj;
+unsigned int tc1200 = (unsigned int)(0.5 * adj_1200 * 1000000.0 / 1200.0);
+unsigned int tc2400 = (unsigned int)(0.5 * adj_2400 * 1000000.0 / 2400.0);
+
+/*
+ * This strings will be used to generate AFSK signals, over and over again.
+ */
+const char *mycall = "KU2Y";
+char myssid = 11;
+
+const char *dest = "APRS";
+
+const char *digi = "WIDE2";
+char digissid = 1;
+
+const char *mystatus = "Hello World !!";
+
+const char *lat = "0610.55S";
+const char *lon = "10649.62E";
+const char sym_ovl = 'H';
+const char sym_tab = 'a';
+
+unsigned int tx_delay = 5000;
+unsigned int str_len = 400;
+
+char bit_stuff = 0;
+unsigned short crc=0xffff;
+
+/*
+ * 
+ */
+void set_nada_1200(void);
+void set_nada_2400(void);
+void set_nada(bool nada);
+
+void send_char_NRZI(unsigned char in_byte, bool enBitStuff);
+void send_string_len(const char *in_string, int len);
+
+void calc_crc(bool in_bit);
+void send_crc(void);
+
+void send_packet(char packet_type);
+void send_flag(unsigned char flag_len);
+void send_header(void);
+void send_payload(char type);
+
+void set_io(void);
+void print_code_version(void);
+void print_debug(char type);
