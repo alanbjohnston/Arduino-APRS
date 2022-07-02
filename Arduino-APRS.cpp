061371e4f581a6b1c6bc177da375f89e4029a3f2
alanbjohnston <alan.b.johnston@gmail.com>
2022-07-02 09:07:23 -0400

    Update Arduino-APRS.cpp

--- Arduino-APRS.cpp
+++ Arduino-APRS.cpp
@@ -22,6 +22,67 @@
 #include "Arduino-APRS.h"
 #include <Arduino.h>
 
+bool nada = _2400;
+
+/*
+ * SQUARE WAVE SIGNAL GENERATION
+ * 
+ * baud_adj lets you to adjust or fine tune overall baud rate
+ * by simultaneously adjust the 1200 Hz and 2400 Hz tone,
+ * so that both tone would scales synchronously.
+ * adj_1200 determined the 1200 hz tone adjustment.
+ * tc1200 is the half of the 1200 Hz signal periods.
+ * 
+ *      -------------------------                           -------
+ *     |                         |                         |
+ *     |                         |                         |
+ *     |                         |                         |
+ * ----                           -------------------------
+ * 
+ *     |<------ tc1200 --------->|<------ tc1200 --------->|
+ *     
+ * adj_2400 determined the 2400 hz tone adjustment.
+ * tc2400 is the half of the 2400 Hz signal periods.
+ * 
+ *      ------------              ------------              -------
+ *     |            |            |            |            |
+ *     |            |            |            |            |            
+ *     |            |            |            |            |
+ * ----              ------------              ------------
+ * 
+ *     |<--tc2400-->|<--tc2400-->|<--tc2400-->|<--tc2400-->|
+ *     
+ */
+const float baud_adj = 0.975;
+const float adj_1200 = 1.0 * baud_adj;
+const float adj_2400 = 1.0 * baud_adj;
+unsigned int tc1200 = (unsigned int)(0.5 * adj_1200 * 1000000.0 / 1200.0);
+unsigned int tc2400 = (unsigned int)(0.5 * adj_2400 * 1000000.0 / 2400.0);
+
+/*
+ * This strings will be used to generate AFSK signals, over and over again.
+ */
+const char *mycall = "KU2Y";
+char myssid = 11;
+
+const char *dest = "APRS";
+
+const char *digi = "WIDE2";
+char digissid = 1;
+
+const char *mystatus = "Hello World !!";
+
+const char *lat = "0610.55S";
+const char *lon = "10649.62E";
+const char sym_ovl = 'H';
+const char sym_tab = 'a';
+
+unsigned int tx_delay = 5000;
+unsigned int str_len = 400;
+
+char bit_stuff = 0;
+unsigned short crc=0xffff;
+
 
 /*
  * 
